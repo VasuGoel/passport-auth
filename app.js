4497dd7d9a36946d731fbd30127b72ff2d3eabc0
Vasu Goel <goelvasu0712@gmail.com>
2019-04-16 16:47:22 +0000

    Render login form and handle login process with passport.authenticate

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const express = require("express"),
       bodyParser = require("body-parser"),
       User = require("./models/user"),
       LocalStrategy = require("passport-local"),
-      passportLocalMongoose = require("passport-local-mongoose");
-      
-      
+      passportLocalMongoose = require("passport-local-mongoose")
+    
 mongoose.connect("mongodb://localhost/auth", {useNewUrlParser: true});
 const app = express();
 app.set('view engine', 'ejs');
@@ -24,7 +23,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//============
 // ROUTES
+//============
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -53,6 +54,25 @@ app.post("/signup", (req, res) => {
     });
 });
 
+// LOGIN ROUTES
+//render login form
+app.get("/login", (req, res) => {
+   res.render("login"); 
+});
+//login logic
+//middleware
+app.post("/login", passport.authenticate("local", {
+    // successRedirect: "/secret",
+    failureRedirect: "/login"
+}) , (req, res) => {
+    res.redirect("/secret/" + req.body.username);
+});
+
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
+
 
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
@@ -60,3 +80,8 @@ function isLoggedIn(req, res, next){
     }
     res.redirect("/login");
 }
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started...");
+})
